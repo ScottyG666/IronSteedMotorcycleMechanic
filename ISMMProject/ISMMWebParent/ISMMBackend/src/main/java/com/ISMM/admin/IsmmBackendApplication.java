022edcd8f2b7a3d0fe399bddc61754569ca8184e
ScottyP666 <Scott_M_G@live.com>
2022-06-22 00:07:35 -0700

    Initializing Multi-Module Application
    This is the Initialization for the Iron Steed Motorcycle Mechanic application.
    
    There will be a customer facing front end which allows people to interact with the website, create a profile, add a motorcycle, and schedule a time and date for which to have the motorcycle seviced.
    
    The back end will allow Administration of the website, such as : Adding new employees, editing employee information, approving customer appointments, and some modularity with website information

---
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/IsmmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ISMM.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmmBackendApplication.class, args);
+	}
+
+}
---
+++ ISMMProject/ISMMWebParent/ISMMFrontend/src/main/java/com/ISMM/site/IsmmFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ISMM.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmmFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmmFrontendApplication.class, args);
+	}
+
+}
