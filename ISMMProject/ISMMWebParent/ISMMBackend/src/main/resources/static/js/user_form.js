ddd10fc72a53dbbdf79ce910d116c92e9e2f7755
ScottyP666 <63470831+ScottyP666@users.noreply.github.com>
2022-06-27 14:42:38 -0700

    Edit User Creation form
    Adding an Eye icon to the Password field of the User Creation form. When clicked it triggers an event listener that:
       +Changed the Eye-Icon into an Eye-Slash-Icon and vice-versa
        +Changes the type of the Password Input to text so it will be readable to the user to ensure correct spelling, and vice-versa

--- ISMMProject/ISMMWebParent/ISMMBackend/src/main/resources/static/js/user_form.js
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/resources/static/js/user_form.js
@@ -1,10 +1,11 @@
+var eyeIcon = document.querySelector('.fa-eye');
 
-
-
-/*
-$(document).ready( () => {
-    $("#buttonCancel").on("click" , () => {
-        window.location = "[[${/users}]]"
-    })
-}) 
-*/
+eyeIcon.addEventListener('click', () => {
+    if (eyeIcon.classList.contains('fa-eye')) {
+        eyeIcon.classList.replace('fa-eye', 'fa-eye-slash');
+        document.querySelector("#password").type = 'text';
+    } else {
+        eyeIcon.classList.replace('fa-eye-slash', 'fa-eye');
+        document.querySelector("#password").type = 'password';
+    }
+});
