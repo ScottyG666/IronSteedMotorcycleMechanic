aceedf91c3b46bf08ce9e477f3aad964369d4ba2
ScottyP666 <Scott_M_G@live.com>
2022-07-15 08:23:57 -0700

    Update user_validation.js

--- ISMMProject/ISMMWebParent/ISMMBackend/src/main/resources/static/js/user_validation.js
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/resources/static/js/user_validation.js
@@ -1,10 +1,8 @@
 var emailInputBox =  document.querySelector('#emailInput');
 
-emailInputBox.addEventListener('blur' , checkIfEmailUnique)
-
-function checkIfEmailUnique () {
+emailInputBox.addEventListener('blur' , () => {
 	var CSRFToken = document.querySelector('input[name$="_csrf"]').value;
-	var userEmail = document.querySelector('#emailInput').value;
+	var userEmail = emailInputBox.value;
 
 	console.log(userEmail)
 
@@ -22,12 +20,40 @@ function checkIfEmailUnique () {
 	})
 	.then((responseEntity) => responseEntity.json())
 	.then( (responseData) => {
-		console.log(responseData)
+		responseData
+	})
+})
+
+
+
+
+/*
+function checkIfEmailUnique () {
+	var CSRFToken = document.querySelector('input[name$="_csrf"]').value;
+	var userEmail = emailInputBox.value;
+
+	console.log(userEmail)
+
+	var user = {
+		"email" : userEmail
+	}
+
+	fetch('/ISMMAdmin/users/check_email' , {
+		method : 'POST' ,
+		headers : {
+			'Content-Type' : 'application/json' ,
+			'X-CSRF-Token' : CSRFToken
+		},
+		body : JSON.stringify(user)
+	})
+	.then((responseEntity) => responseEntity.json())
+	.then( (responseData) => {
+		responseData
 	})
 }
 
 
-
+*/
 
 
 
