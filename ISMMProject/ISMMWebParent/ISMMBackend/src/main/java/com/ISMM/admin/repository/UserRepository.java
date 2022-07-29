cf4aead427e365c58906eaeb600d230cfb1a1d16
ScottyP666 <Scott_M_G@live.com>
2022-07-29 01:03:42 -0700

    Update UserRepository.java
    Refactor of the findAll query using the CONCAT operator to query for anything that may be searched for with the addition on a space between each possible query field

--- ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/repository/UserRepository.java
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ISMM.admin.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,8 +19,18 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	public Long countById(Integer id) ;
 	
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
+			+ " u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
+	/*
+	 *  u.firstName LIKE %?1% OR u.lastName LIKE %?1%" 
+			+ " OR u.email LIKE %?1%")
+	 */
 	
-	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+	
+	
+	
+	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1 ")
 	@Modifying
 	public void updateEnabledStatus (Integer id, Boolean enabled);
 	
