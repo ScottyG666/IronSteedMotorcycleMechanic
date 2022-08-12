5558190ae8c7973e937090b06050629785892c16
ScottyG666 <Scott_M_G@live.com>
2022-08-12 08:48:57 -0700

    Adding Authentication capabilities
    Creation of ISMMUserDetails extending the Userdetails and ISMMUserDetailsService extending UserDetailsService from spring security core in order to pass into an authentication provider to determine the level of access granted per user based on the roles
    
    Adding bad credentials/logout message to login page for better user experience
    
    Fetching the roles eagerly for use in authentication

---
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/security/service/ISMMUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ISMM.admin.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ISMM.admin.repository.UserRepository;
+import com.ISMM.admin.security.ISMMUserDetails;
+import com.ISMM.common.domain.User;
+
+@Service
+public class ISMMUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.getUserByEmail(email);
+		
+		if (user != null) {
+			return new ISMMUserDetails(user);
+		}
+
+		throw new UsernameNotFoundException("Could not find user with email: " + email);
+	}
+}
