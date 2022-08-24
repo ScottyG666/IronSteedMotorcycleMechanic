3310209b9e47c54c89b949a806e962ad11714c92
ScottyG666 <63470831+ScottyG666@users.noreply.github.com>
2022-08-23 19:42:04 -0700

    Creation of the CategoryRepositoryTest
    Writing tests to create/persist Objects within the categoyRepository that are saved to the database due to the AutoConfigureTestDatabase annotation.
    
    Tests include :
      -Creating categories for incentory items that will exist in the inventory
      -creating sub categories based on the parent foreign key
      -printing the categories to the console to ensure existence in a hierarchical order.
    
    This update also includes a non-important adjustment to the application.properties file in order to reduce verbosity within the consol.

--- ISMMProject/ISMMWebParent/ISMMBackend/src/test/java/com/ISMM/admin/category/CategoryRepositoryTest.java
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/test/java/com/ISMM/admin/category/CategoryRepositoryTest.java
@@ -3,6 +3,7 @@ package com.ISMM.admin.category;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import org.springframework.test.annotation.Rollback;
 import com.ISMM.admin.repository.CategoryRepository;
 import com.ISMM.common.domain.Category;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class CategoryRepositoryTest {
@@ -24,23 +25,110 @@ public class CategoryRepositoryTest {
 	
 	@Test
 	public void testCreateRootCategory() {
-		Category category = new Category("Exhaust");
-		Category savedCategory = catRepo.save(category);
 		
-		assertThat(savedCategory.getId()).isGreaterThan(0);
+		 	Category category = new Category("Computer");
+			Category savedCategory = catRepo.save(category);
+			
+			assertThat(savedCategory.getId()).isGreaterThan(0);
+			
+	
+			Category secondCategory = new Category("Electronics");
+			Category secondarySavedCategory = catRepo.save(secondCategory);
+			
+			assertThat(secondarySavedCategory.getId()).isGreaterThan(0);
+		
 	}
 	
-	//LEFT OFF @ 11:30 VIDEO 68
 	
 	@Test
-	public void createSubCategory() {
-		Category parent = new Category(1);
-		//Category subCategory = new Category("Desktop", parent);
-		Category laptops = new Category("Laptops", parent);
-		Category computerComponents= new Category("Computer Components", parent);
+	public void createSubCategorys() {
+		Category primaryParent = new Category(1);
+		Category primarySubCategory = new Category("Desktop", primaryParent);
+		Category primarySavedCategory = catRepo.save(primarySubCategory);
+		assertThat(primarySavedCategory.getId()).isGreaterThan(0);
 		
-		catRepo.saveAll(List.of(laptops, computerComponents));
 		
-		//assertThat(savedCategory.getId()).isGreaterThan(0);
+		Category laptops = new Category("Laptops", primaryParent);
+		Category components = new Category("Computer Components", primaryParent);
+		catRepo.saveAll(List.of(laptops, components));
+		 
+		
+		Category secondaryParent = new Category(2);
+
+		Category cameras = new Category("Cameras", secondaryParent);
+		Category smartphones = new Category("Smartphones", secondaryParent);
+		catRepo.saveAll(List.of(cameras, smartphones));
 	}
+	
+	
+	
+	@Test 
+	public void creatingChildSubcategory () {
+		Category parentSubCategory = new Category(5);
+		Category childSubCategory = new Category("Memory", parentSubCategory);
+		
+		Category savedSubCategoryChild = catRepo.save(childSubCategory);
+		assertThat(savedSubCategoryChild.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void createAdditionalChildSubCategorys() {
+		
+		Category smartphoneParent = new Category(7);
+		Category smartphoneSubCategory = new Category("IPhone", smartphoneParent);
+		Category savedSmartphoneSubCategory = catRepo.save(smartphoneSubCategory);
+		assertThat(savedSmartphoneSubCategory.getId()).isGreaterThan(0);
+		
+		Category laptopParent = new Category(4);
+		Category laptopSubCategory = new Category("Gaming Laptops", laptopParent);
+		Category savedLaptopSubCategory = catRepo.save(laptopSubCategory);
+		assertThat(savedLaptopSubCategory.getId()).isGreaterThan(0);
+		
+		
+	}
+
+	
+	@Test
+	public void testGetCategory () {
+		Category category = catRepo.findById(2).get();
+		System.out.println(category.getName());
+		
+		Set<Category> children = category.getChildren();
+		for (Category subCategory : children) {
+			System.out.println(subCategory.getName());
+		}
+		assertThat(children.size()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testPrintHierarchicalCategories () {
+		Iterable<Category> categories = catRepo.findAll();
+		
+		for(Category category: categories ) {
+			if(category.getParent() == null) {
+				System.out.println(category.getName());
+				
+				Set<Category> children = category.getChildren();
+				
+				for (Category subCategory : children) {
+					System.out.println("--" + subCategory.getName());
+					printChildren(subCategory, 1);
+				}
+			}
+		}
+	}
+	
+	private void printChildren(Category parent, int sublevel) {
+		int newSubLevel = sublevel + 1;
+		
+		Set<Category> children = parent.getChildren();
+		
+		for (Category subCategory : children) {
+			for (int i = 0; i < newSubLevel;i++) {
+				System.out.print("--");
+			}
+			System.out.println(subCategory.getName());
+		}
+	}
+	
 }
