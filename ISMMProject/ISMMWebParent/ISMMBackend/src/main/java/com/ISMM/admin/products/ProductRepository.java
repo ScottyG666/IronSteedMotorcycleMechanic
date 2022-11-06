8a8eae633c38ef69541c2bfadb2dbcd9354d65a5
ScottyG666 <Scott_M_G@live.com>
2022-11-06 14:05:55 -0800

    Product Object and Repository

---
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/products/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.ISMM.admin.products;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ISMM.common.domain.Product;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+}
