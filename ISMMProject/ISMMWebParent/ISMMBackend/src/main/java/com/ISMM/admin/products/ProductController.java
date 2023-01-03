c7a14019bad72bdbaceaae82b060a38beb2c035d
ScottyG666 <Scott_M_G@live.com>
2023-01-02 19:34:52 -0800

    Adding the Save functionality to Product form
    Adding the GetMapping for the save function of the Product Form.
    The controller's Mapping for saving takes advantage of a ProductService.save() method that also does 2 things:
      1) checks to see if the Alias field is empty, in which case the Product Name field is used as the alias.
      2) if the Alias field is filled, the provided text it checked for any spaces and replaces them with dashes.
    --In both cases the alias field is stripped of any spaces between words and substituted with dashed. This is done because the alias is used within the URL and spaces would be detrimental to this.

--- ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/products/ProductController.java
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/products/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ISMM.admin.brands.BrandService;
 import com.ISMM.common.domain.Brand;
@@ -50,10 +51,9 @@ public class ProductController {
 	}
 
 	@PostMapping("/save")
-	public String saveProduct(Product product) {
-		System.out.println("Product Name: " + product.getName());
-		System.out.println("Brand ID: " + product.getBrand().getId());
-		System.out.println("Category ID: " + product.getCategory().getId());
+	public String saveProduct(Product product, RedirectAttributes ra) {
+		prodService.save(product);
+		ra.addFlashAttribute("message", "The product has been saved successfully.");
 
 		return "redirect:/products";
 	}
--- ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/products/ProductService.java
+++ ISMMProject/ISMMWebParent/ISMMBackend/src/main/java/com/ISMM/admin/products/ProductService.java
@@ -1,5 +1,6 @@
 package com.ISMM.admin.products;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,32 @@ public class ProductService {
 
 	public List<Product> listAll() {
 		return (List<Product>) repo.findAll();
+	}
+	
+	
+	/**
+	 * Saves the product into the database. If the Alias field is left
+	 * 	blank during the creation of a new product, the Alias will assume
+	 * 	the Product Name with the spaces replaced by dashes, as the 
+	 * 	Alias will be used within the URL
+	 * 
+	 * @param product
+	 * @return product
+	 */
+	public Product save(Product product) {
+		if (product.getId() == null) {
+			product.setCreatedTime(new Date());
+		}
+
+		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+			String defaultAlias = product.getName().replaceAll(" ", "-");
+			product.setAlias(defaultAlias);
+		} else {
+			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+		}
+
+		product.setUpdatedTime(new Date());
+
+		return repo.save(product);
 	}
 }
